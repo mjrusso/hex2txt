b4a6488e576960b4a5159eb96b567ade7886867d
Michael Russo <mjr@mjrusso.com>
2024-12-15 08:46:26 -0500

    Add Nix Flake.

---
+++ flake.nix
@@ -0,0 +1,77 @@
+{
+  description = "Nix-Flake-based development environment for hex2txt";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    next-ls = {
+      url = "github:elixir-tools/next-ls";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, next-ls }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        erlang = pkgs.beam.interpreters.erlang_27;
+        pkgs-beam = pkgs.beam.packagesWith erlang;
+        elixir = pkgs-beam.elixir_1_17;
+        elixir-ls = pkgs-beam.elixir-ls;
+        node = pkgs.nodejs_20;
+
+        base-packages = [ erlang elixir pkgs.git node pkgs.just ];
+
+        elixir-language-servers =
+          [ elixir-ls next-ls.packages.${system}.default ];
+
+        base-scripts = [ ];
+
+      in {
+
+        # A stripped-down dev shell, for use in CI environments.
+        #
+        # Example usage:
+        #
+        #     nix develop .#ci -c COMMAND
+        #
+        # This shell is faster to build than the default, because it doesn't
+        # include the Elixir language servers and other dependencies that are
+        # only useful for development.
+        devShells.ci =
+          pkgs.mkShell { packages = base-packages ++ base-scripts; };
+
+        devShells.default = pkgs.mkShell {
+          packages = base-packages ++ elixir-language-servers ++ base-scripts
+
+            ++ pkgs.lib.optionals pkgs.stdenv.isLinux
+            (with pkgs; [ inotify-tools libnotify ])
+
+            ++ pkgs.lib.optionals pkgs.stdenv.isDarwin
+            (with pkgs.darwin.apple_sdk.frameworks; [
+              pkgs.terminal-notifier
+              CoreFoundation
+              CoreServices
+            ]);
+
+          shellHook = ''
+            export PATH=$PWD/assets/node_modules/.bin:$PATH
+
+            # Store configuration files and scripts used by Mix in this local directory.
+            mkdir -p .nix-shell/.mix
+            export MIX_HOME=$PWD/.nix-shell/.mix
+            export PATH=$MIX_HOME/bin:$PATH
+            export PATH=$MIX_HOME/escripts:$PATH
+
+            # Store cache and configuration files used by Hex in this local directory.
+            mkdir -p .nix-shell/.hex
+            export HEX_HOME=$PWD/.nix-shell/.hex
+            export PATH=$HEX_HOME/bin:$PATH
+
+            # Enable shell history for IEx.
+            export ERL_AFLAGS="-kernel shell_history enabled -kernel shell_history_path '\"$PWD/.nix-shell/.erlang-history\"'"
+          '';
+
+        };
+      });
+
+}
